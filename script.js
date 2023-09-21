e9c7534852326853ad5684889106df476fee0029
Ron <newtonron12@gmail>
2023-09-20 22:11:02 -0500

    added questions

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 
 
 
+
+
+const questions = [
+    {
+        question: "The only true wisdom is knowing you know nothing",
+        choices: ["Socrates", "Nietzche", "John Locke", "Plato"],
+        correctAnswer: "Socrates",
+    },
+    {
+        question: "Experience without theory is blind, but theory without experience is mere intellectual play.",
+        choices: ["Lao Tzu", "John Locke", "Alan Watts", "Immanuel Kant"],
+        correctAnswer: "Immanuel Kant",
+    },
+    {
+        question: "Who said, 'I think, therefore I am'?",
+        choices: ["Kuhn", "Socrates", "Descartes", "Plato"],
+        correctAnswer: "Descartes",
+    },
+    
+];
+
+
+
+
+
 // hide quiz and and results section on load
 
 window.addEventListener('load', () =>  {
